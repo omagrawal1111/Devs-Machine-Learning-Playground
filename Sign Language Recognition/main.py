ab0e27b00fcfd14efab78a25f5296f598ba69d96
Om Agrawal <omagrawal1111@gmail.com>
2021-05-01 17:41:08 +0530

    Update main.py

--- Sign Language Recognition/main.py
+++ Sign Language Recognition/main.py
@@ -77,3 +77,81 @@ print(Y_train.shape)
 print(Y_validate.shape)
 print(Y_test.shape)
 # %%
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1/255,rotation_range=45, width_shift_range=0.25,
+    height_shift_range=0.15,shear_range=0.15, zoom_range=0.2, fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1/255)
+valid_datagen = ImageDataGenerator(rescale=1/255)
+train_generator = train_datagen.flow(X_train, Y_train, batch_size=32)
+test_generator =  test_datagen.flow(X_test,Y_test,batch_size=32)
+valid_generator = valid_datagen.flow(X_validate,Y_validate,batch_size=32)
+
+
+# %%
+import tensorflow as tf
+
+model = tf.keras.Sequential(
+[
+    tf.keras.layers.Conv2D(16, (3,3), padding='same', activation=tf.nn.relu,
+                           input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2,2)),
+     tf.keras.layers.Conv2D(128, (3,3), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(num_classes, activation=tf.nn.softmax)
+])
+
+# %%
+#!!!!! IF YOU USE ADAM , THE ACCURACY STAYS AT 0 , USE SGD OPTIMIZER !!!
+model.compile(optimizer='SGD',loss='categorical_crossentropy',metrics = ['accuracy'])
+model.summary()
+
+
+# %%
+history = model.fit(train_generator,
+                    epochs=500,
+                    validation_data=valid_generator,
+                    callbacks = [
+    tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10),
+    tf.keras.callbacks.ModelCheckpoint(filepath='/kaggle/working/',
+    monitor='val_accuracy',
+    save_best_only=True)
+])
+
+# %%
+# Plot the chart for accuracy and loss on both training and validation
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+acc = history.history['accuracy']# Your Code Here
+val_acc =history.history['val_accuracy'] # Your Code Here
+loss = history.history['loss']# Your Code Here
+val_loss = history.history['val_loss']# Your Code Here
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# %%
+score = model.evaluate(test_generator, verbose = 0) 
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# %%
